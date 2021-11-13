328cc68dcceb7a01e6acff90b62a66d2550b1429
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-13 13:10:47 +0100

    Paso de alcanzar intentos maximos

--- terminar_juego.py
+++ terminar_juego.py
@@ -3,22 +3,27 @@ MIN = 0  #Defino los parametros entre los que quiero que trabaje la funcion
 MAX = 0
 minimo = MIN
 maximo = MAX
-numero_elegido = random.randint(minimo,maximo)
+intentos_maximos = 0
 
 while True:
     nivel = input ("Seleccione un nivel: facil, medio, avanzado, o maestro:")
     if nivel == "facil":
             maximo = 100
+            intentos_maximos = 10
             break
     elif nivel == "medio":
             maximo = 1000
+            intentos_maximos = 20
             break
     elif nivel == "avanzado":
             maximo = 1000000
+            intentos_maximos = 50
     elif nivel == "maestro":
             maximo = 1000000000000
+            intentos_maximos = 100
             break
-    
+numero_elegido = random.randint(minimo,maximo)
+
 def pedir_numero(invitacion):
     invitacion += " entre " + str(minimo) + " y " + str(maximo) + " :"  #no entiendo muy bien el uso de invitacion
     while True:
@@ -38,28 +43,30 @@ def pedir_numero(invitacion):
 def ayudas():
     ayuda = input (print("¿Quiere algo de ayuda? si o no????"))
     try:
-        if ayuda == "si" or ayuda == "no":
+        if ayuda == "si":
             print("El numero esta entre " + str(minimo) + " y " + str(maximo))
         else:
             print("Tu te lo pierdes...")
     except:
         pass
     return ayuda
-
-# Parte 1
-print("Introduce el numero a adivinar")
-numero = pedir_numero("Introduzca el numero a adivinar",)  #Hace referencia a la funcion definida anteriormente bajo el nombre de pedir_numero()
-
+intentos = 1
 #Parte 2
-print("Intente adivinar el numero")
+print("Que comienze el juego, intenta adivinar el numero aleatorio")
 while True:
     intento = pedir_numero("Intente adivinar el numero") #Aqui tambien se hace referencia a la funcion anterior
-    if intento < numero:
+    if intento < numero_elegido:
         print("Te has quedado corto")
         minimo = intento + 1 #Esto sirve para ayudar a acertar el numero, reduce posibilidades
-    elif intento > numero:
+        intentos += 1
+    elif intento > numero_elegido:
         print("Te has pasado crack")
         maximo = intento - 1
+        intentos += 1
     else:
         print("Enhorabuena campeon, lo has acertado")
+        print("Ha necesitado ", intentos, "intentos para adivinarlo")
         break
+
+if intentos == intentos_maximos:
+    print("No te quedan mas oportunidades, has perdido :(")
