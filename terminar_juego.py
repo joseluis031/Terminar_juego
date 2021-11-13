419244e2e14886df3e5bd5ba9708976d458a342d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-13 11:27:01 +0100

    Reduzco las posibilidades

--- terminar_juego.py
+++ terminar_juego.py
@@ -1,9 +1,10 @@
 
 MIN = 0  #Defino los parametros entre los que quiero que trabaje la funcion 
 MAX = 99
-
+minimo = MIN
+maximo = MAX
 def pedir_numero(invitacion, minimo, maximo):
-    invitacion += "entre" + str(minimo) + "y" + str(maximo) + ":"  #no entiendo muy bien el uso de invitacion
+    invitacion += " entre " + str(minimo) + " y " + str(maximo) + " :"  #no entiendo muy bien el uso de invitacion
     while True:
         entrada = input(invitacion)  #Aquí tenemos una función que pide al usuario que introduzca un número cualquiera
         try:
@@ -22,11 +23,13 @@ numero = pedir_numero("Introduzca el numero a adivinar", MIN, MAX)  #Hace refere
 #Parte 2
 print("Intente adivinar el numero")
 while True:
-    intento = pedir_numero("Intente adivinar el numero", MIN, MAX) #Aqui tambien se hace referencia a la funcion anterior
+    intento = pedir_numero("Intente adivinar el numero", minimo, maximo) #Aqui tambien se hace referencia a la funcion anterior
     if intento < numero:
         print("Te has quedado corto")
+        minimo = intento + 1 #Esto sirve para ayudar a acertar el numero, reduce posibilidades
     elif intento > numero:
         print("Te has pasado crack")
+        maximo = intento - 1
     else:
         print("Enhorabuena campeon, lo has acertado")
         break
