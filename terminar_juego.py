e8ab89108ed01156ebcd938ccff982413609d5f8
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-13 11:16:20 +0100

    parametros

--- terminar_juego.py
+++ terminar_juego.py
@@ -2,27 +2,27 @@
 MIN = 0  #Defino los parametros entre los que quiero que trabaje la funcion 
 MAX = 99
 
-def pedir_numero(invitacion):
-    invitacion += "entre" + str(MIN) + "y" + str(MAX) + ":"  #no entiendo muy bien el uso de invitacion
+def pedir_numero(invitacion, minimo, maximo):
+    invitacion += "entre" + str(minimo) + "y" + str(maximo) + ":"  #no entiendo muy bien el uso de invitacion
     while True:
-        entrada = input("Introduzca un numero entre 0 y 99:")  #Aquí tenemos una función que pide al usuario que introduzca un número cualquiera
+        entrada = input(invitacion)  #Aquí tenemos una función que pide al usuario que introduzca un número cualquiera
         try:
             entrada = int(entrada)
         except:
             pass
         else:
-            if 0 <= entrada <= 99:
+            if minimo <= entrada <= maximo:
                 break
     return entrada
 
 # Parte 1
 print("Introduce el numero a adivinar")
-numero = pedir_numero()  #Hace referencia a la funcion definida anteriormente bajo el nombre de pedir_numero()
+numero = pedir_numero("Introduzca el numero a adivinar", MIN, MAX)  #Hace referencia a la funcion definida anteriormente bajo el nombre de pedir_numero()
 
 #Parte 2
 print("Intente adivinar el numero")
 while True:
-    intento = pedir_numero() #Aqui tambien se hace referencia a la funcion anterior
+    intento = pedir_numero("Intente adivinar el numero", MIN, MAX) #Aqui tambien se hace referencia a la funcion anterior
     if intento < numero:
         print("Te has quedado corto")
     elif intento > numero:
