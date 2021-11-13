eca322f1f4813a57cb4929051c743bc320a9e041
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-13 10:41:41 +0100

    primera funcion

--- terminar_juego.py
+++ terminar_juego.py
@@ -9,3 +9,19 @@ def pedir_numero():
             if 0 <= entrada <= 99:
                 break
     return entrada
+
+# Parte 1
+print("Introduce el numero a adivinar")
+numero = pedir_numero()  #Hace referencia a la funcion definida anteriormente bajo el nombre de pedir_numero()
+
+#Parte 2
+print("Intente adivinar el numero")
+while True:
+    intento = pedir_numero() #Aqui tambien se hace referencia a la funcion anterior
+    if intento < numero:
+        print("Te has quedado corto")
+    elif intento > numero:
+        print("Te has pasado crack")
+    else:
+        print("Enhorabuena campeon, lo has acertado")
+        break
